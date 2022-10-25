97637710b949f276e62e8ffa38a68d35a45df2d5
rajibdas-it <rajibrad@gmail.com>
2022-10-26 02:00:06 +0600

    details page complete

--- src/Components/CourseDetails/CourseDetails.js
+++ src/Components/CourseDetails/CourseDetails.js
@@ -5,8 +5,20 @@ const CourseDetails = () => {
   const courseDetails = useLoaderData();
   const { id, title, descriptions, price, rating, img } = courseDetails;
   return (
-    <div>
+    <div className="w-[85%] mx-auto mt-20">
       <h1>Course Details Page of {title}</h1>
+      <div className="card card-compact w-full bg-base-100 shadow-xl">
+        <figure>
+          <img className="w-full" src={img} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{descriptions}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
       <button className="btn btn-outline">Download</button>
     </div>
   );
