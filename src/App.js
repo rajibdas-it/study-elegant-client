4aeaf2b08cec401b59784ec16a08ec171814f7ef
rajibdas-it <rajibrad@gmail.com>
2022-10-26 11:45:58 +0600

    email, password and google login success

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import { router } from "./Routes/Routes";
 
 function App() {
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer position="top-center" />
     </div>
   );
 }
--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../Firebase/firebase.config";
 
-const UserContext = () => {
-  return <div></div>;
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const UserContext = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = (googleProvier) => {
+    return signInWithPopup(auth, googleProvier);
+  };
+
+  const authInfo = { createUser, googleSignIn };
+  return (
+    <div>
+      <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    </div>
+  );
 };
 
 export default UserContext;
