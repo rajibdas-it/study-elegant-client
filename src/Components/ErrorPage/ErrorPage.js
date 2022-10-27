c10c8cbdf2aaddf6f66509c5596be2da5a846db9
rajibdas-it <rajibrad@gmail.com>
2022-10-27 16:46:13 +0600

    error page done

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -1,9 +1,37 @@
+import { LottiePlayer } from "lottie-react";
 import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import Lottie from "lottie-react";
+import errorImg from "../../assets/error.gif";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
   return (
-    <div>
-      <h1>4O4 Page no found</h1>
+    <div className=" w-[70%] mx-auto">
+      <div className="w-[60%] mx-auto mt-5">
+        <img src={errorImg} alt="" />
+      </div>
+      <div>
+        {error && (
+          <div>
+            <h1 className="text-center text-5xl font-bold italic mb-5">
+              {error.status}
+            </h1>
+            <p className="text-center text-lg font-bold italic">
+              {error.statusText || error.message}
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className="text-center mt-5">
+        <Link to="/">
+          <button className="btn btn-primary capitalize bg-gradient-to-tr from-purple-400 to-blue-400 text-white border-none hover:from-blue-400">
+            Back To Home Page
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
